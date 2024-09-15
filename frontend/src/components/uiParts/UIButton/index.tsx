603734be7efbf4cdf70a7908050363e2e5c70892
masaokaka <osushiiisensu@gmail.com>
2024-09-15 22:34:44 +0900

    UIButtonコンポーネント修正

--- frontend/src/components/uiParts/UIButton/index.tsx
+++ frontend/src/components/uiParts/UIButton/index.tsx
@@ -4,37 +4,36 @@ import type { PropsWithChildren } from 'react';
 
 type ButtonPropsType = {
   isLink: false;
-  icon?: JSX.Element;
 } & ButtonProps;
 
 type LinkPropsType = {
   isLink: true;
-  icon?: JSX.Element;
 } & LinkProps;
 
-type UIButtonProps = PropsWithChildren<ButtonPropsType | LinkPropsType>;
+type UIButtonProps = ButtonPropsType | LinkPropsType;
 
 // 型ガードでisLinkを判定
 const isLinkProps = (props: UIButtonProps): props is LinkPropsType =>
   props.isLink === true;
 
 // isLinkが渡された場合はlink、それ以外はbutton
-export const UIButton = (props: UIButtonProps): JSX.Element =>
+export const UIButton = ({
+  children,
+  ...props
+}: PropsWithChildren<UIButtonProps>): JSX.Element =>
   isLinkProps(props) ? (
     <Button
       {...props}
       as={Link}
       className="flex h-[40px] items-center rounded-full bg-black px-4 text-sm text-white data-[hover]:bg-gray-800 data-[hover]:data-[active]:bg-gray-900"
     >
-      {props.icon}
-      {props.children}
+      {children}
     </Button>
   ) : (
     <Button
       {...props}
       className="h-[40px] rounded-full bg-black px-4 text-sm text-white data-[hover]:bg-gray-800 data-[hover]:data-[active]:bg-gray-900"
     >
-      {props.icon}
-      {props.children}
+      {children}
     </Button>
   );
