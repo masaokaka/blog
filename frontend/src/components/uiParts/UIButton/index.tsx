5a516c83ecf14109635dad3ae2d56ac5503dc993
masaokaka <osushiiisensu@gmail.com>
2024-09-15 22:13:08 +0900

    UIButtonコンポーネントの作成

--- frontend/src/components/uiParts/UIButton/index.tsx
+++ frontend/src/components/uiParts/UIButton/index.tsx
@@ -1,13 +1,40 @@
-import { Button } from '@headlessui/react';
+import { Button, type ButtonProps } from '@headlessui/react';
+import Link, { type LinkProps } from 'next/link';
+import type { PropsWithChildren } from 'react';
 
-type Props = {
-  text: string;
+type ButtonPropsType = {
+  isLink: false;
   icon?: JSX.Element;
-};
+} & ButtonProps;
 
-export const UIButton = ({ text, icon }: Props): JSX.Element => (
-  <Button className="h-[40px] rounded-full bg-black px-4 text-sm text-white data-[hover]:bg-gray-800 data-[hover]:data-[active]:bg-gray-900">
-    {icon}
-    {text}
-  </Button>
-);
+type LinkPropsType = {
+  isLink: true;
+  icon?: JSX.Element;
+} & LinkProps;
+
+type UIButtonProps = PropsWithChildren<ButtonPropsType | LinkPropsType>;
+
+// 型ガードでisLinkを判定
+const isLinkProps = (props: UIButtonProps): props is LinkPropsType =>
+  props.isLink === true;
+
+// isLinkが渡された場合はlink、それ以外はbutton
+export const UIButton = (props: UIButtonProps): JSX.Element =>
+  isLinkProps(props) ? (
+    <Button
+      {...props}
+      as={Link}
+      className="flex h-[40px] items-center rounded-full bg-black px-4 text-sm text-white data-[hover]:bg-gray-800 data-[hover]:data-[active]:bg-gray-900"
+    >
+      {props.icon}
+      {props.children}
+    </Button>
+  ) : (
+    <Button
+      {...props}
+      className="h-[40px] rounded-full bg-black px-4 text-sm text-white data-[hover]:bg-gray-800 data-[hover]:data-[active]:bg-gray-900"
+    >
+      {props.icon}
+      {props.children}
+    </Button>
+  );
