2786042b26fd170a361c5a91f2ca15dc53380b04
masaokaka <osushiiisensu@gmail.com>
2024-10-21 20:51:03 +0900

    fix: pagination処理の不具合修正

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { useMemo } from 'react';
 
 const firstPage = 1;
+const maxElementCount = 7;
 
 const Pagination = ({
   totalPage,
@@ -11,17 +12,17 @@ const Pagination = ({
   totalPage: number;
   currentPage: number;
 }) => {
-  const pages = useMemo((): ('...' | number)[] => {
-    // 総ページ数が6ページ以下の場合、ページ全てを描画
-    if (totalPage <= 6) {
+  const paginationElements = useMemo((): ('...' | number)[] => {
+    // 総ページ数が7ページ以下の場合、ページ全てを描画
+    if (totalPage <= maxElementCount) {
       return Array.from({ length: totalPage }, (_, i) => i + 1);
     }
-    // 総ページ数が6以上で現在のページと最初のページとの差が2以下の場合
+    // 総ページ数が7以上で現在のページと最初のページとの差が2以下の場合
     if (currentPage - firstPage <= 3) {
-      return [1, 2, 3, 4, '...', totalPage];
+      return [1, 2, 3, 4, 5, '...', totalPage];
     }
-    // 総ページ数が6以上で現在のページと最初のページとの差が3以上、最後のページとの差が3より上
-    if (currentPage - firstPage >= 3 && totalPage - currentPage > 2) {
+    // 総ページ数が7以上で現在のページと最初のページとの差が3以上、最後のページとの差が3より上
+    if (currentPage - firstPage >= 3 && totalPage - currentPage > 3) {
       return [
         1,
         '...',
@@ -32,12 +33,20 @@ const Pagination = ({
         totalPage,
       ];
     }
-    // 総ページ数6以上、現在のページが最後のページと3以内
-    return [1, '...', totalPage - 3, totalPage - 2, totalPage - 1, totalPage];
+    // 総ページ数7以上、現在のページが最後のページと3以内
+    return [
+      1,
+      '...',
+      totalPage - 4,
+      totalPage - 3,
+      totalPage - 2,
+      totalPage - 1,
+      totalPage,
+    ];
   }, [currentPage, totalPage]);
   return (
     <div className="my-5 flex justify-center space-x-2 font-semibold">
-      {pages.map((page, i) => {
+      {paginationElements.map((page, i) => {
         const isDisabled = page === '...';
         const isCurrentPage = page === currentPage;
         return (
@@ -45,9 +54,9 @@ const Pagination = ({
             key={`${i}_page`}
             aria-disabled={isDisabled}
             tabIndex={isDisabled ? -1 : undefined}
-            href={`/blog?page=${page}`}
+            href={isDisabled ? '' : `/blog?page=${page}`}
             className={clsx(
-              'flex size-8 min-w-8 items-center justify-center rounded-full text-center transition-all md:size-10',
+              'flex size-8 min-w-8 items-center justify-center rounded-full text-center md:size-10',
               isCurrentPage && 'pointer-events-none bg-black text-white',
               isDisabled ? 'pointer-events-none' : 'hover:bg-white',
             )}
